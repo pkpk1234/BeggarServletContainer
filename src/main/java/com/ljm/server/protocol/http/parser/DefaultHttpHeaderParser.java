461c3c6f55ae4013e7d2e97a54b8291a5cd190fc
l00388716 <lijiaming@huawei.com>
2018-01-17 18:49:13 +0800

    update

--- src/main/java/com/ljm/server/protocol/http/parser/DefaultHttpHeaderParser.java
+++ src/main/java/com/ljm/server/protocol/http/parser/DefaultHttpHeaderParser.java
@@ -3,6 +3,8 @@ package com.ljm.server.protocol.http.parser;
 import com.ljm.server.protocol.http.HttpHeader;
 import com.ljm.server.protocol.http.HttpHeaderParser;
 import com.ljm.server.protocol.http.header.HttpMessageHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 import java.util.Scanner;
@@ -15,7 +17,7 @@ public class DefaultHttpHeaderParser implements HttpHeaderParser {
     private static final String SPLITTER = ":";
     private static final String CR = "\r";
     private static final String LF = "\n";
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultHttpHeaderParser.class);
     @Override
     public HttpMessageHeaders parser(InputStream inputStream) {
         HttpMessageHeaders httpMessageHeaders = new HttpMessageHeaders();
@@ -25,9 +27,10 @@ public class DefaultHttpHeaderParser implements HttpHeaderParser {
         while (scanner.hasNextLine()) {
             String keyValue = scanner.nextLine();
             //只解析Http Header
-            if (keyValue.equals(CR + LF) || keyValue.equals(CR) || keyValue.equals("LF")) {
+            if (keyValue.equals("")) {
                 break;
             }
+            LOGGER.info("key value {}",keyValue);
             String[] temp = keyValue.split(SPLITTER);
             if (temp.length == 2) {
                 httpMessageHeaders.addHeader(new HttpHeader(temp[0], temp[1].trim()));
