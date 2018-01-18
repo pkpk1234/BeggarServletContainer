2d0ac9a1db62437c806aaba4a701aa5676623a58
lijiaming <lijiaming1984@gmail.com>
2018-01-18 22:54:30 +0800

    添加注释

--- src/main/java/com/ljm/server/protocol/http/parser/AbstractHttpRequestMessageParser.java
+++ src/main/java/com/ljm/server/protocol/http/parser/AbstractHttpRequestMessageParser.java
@@ -30,17 +30,26 @@ public abstract class AbstractHttpRequestMessageParser extends AbstractParser im
      */
     @Override
     public HttpMessage parse(InputStream inputStream) throws IOException {
-        //设置上下文
+        //1.设置上下文
         getAndSetBytesToContext(inputStream);
+        //2.解析构造RequestLine
         RequestLine requestLine = parseRequestLine();
+        //3.解析构造QueryParameters
         HttpQueryParameters httpQueryParameters = parseHttpQueryParameters();
+        //4.解析构造HTTP请求头，并设置是否有body，如果有则body段之前byte长度到上下文中
         IMessageHeaders messageHeaders = parseRequestHeaders();
+        //5.解析构造HTTP Body，如果有个的话
         Optional<HttpBody<?>> httpBody = parseRequestBody();
 
         HttpRequestMessage httpRequestMessage = new HttpRequestMessage(requestLine, messageHeaders, httpBody, httpQueryParameters);
         return httpRequestMessage;
     }
 
+    /**
+     * 读取请求发送的数据，并保存为byte数组设置到解析上下文中
+     * @param inputStream
+     * @throws IOException
+     */
     private void getAndSetBytesToContext(InputStream inputStream) throws IOException {
         byte[] bytes = IOUtils.toByteArray(new InputStreamReader(inputStream), "utf-8");
         super.httpParserContext.setHttpMessageBytes(bytes);
