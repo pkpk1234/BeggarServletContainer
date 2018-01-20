5823f424bdd5b78ab9b5808c529227d4cb29b9c9
lijiaming <lijiaming1984@gmail.com>
2018-01-20 10:11:11 +0800

    update

--- src/test/java/com/ljm/server/protocol/http/parser/TestDefaultHttpQueryParameterParser.java
+++ src/test/java/com/ljm/server/protocol/http/parser/TestDefaultHttpQueryParameterParser.java
@@ -20,7 +20,7 @@ public class TestDefaultHttpQueryParameterParser {
         String queryStr = "a=123&a1=1&b=456&a=321";
         DefaultHttpQueryParameterParser httpRequestParameterParser
                 = new DefaultHttpQueryParameterParser();
-        HttpQueryParameters result = httpRequestParameterParser.parse(queryStr);
+        HttpQueryParameters result = httpRequestParameterParser.parse();
         List<HttpQueryParameter> parameters = result.getQueryParameter("a");
         assertNotNull(parameters);
         assertEquals(2, parameters.size());
