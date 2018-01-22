b8c112be5280e6e42e4527e0c0ab8a325fa1e98c
l00388716 <lijiaming@huawei.com>
2018-01-22 13:10:47 +0800

    update

--- src/main/java/com/ljm/server/protocol/handler/HttpStaticResourceEventHandler.java
+++ src/main/java/com/ljm/server/protocol/handler/HttpStaticResourceEventHandler.java
@@ -1,4 +1,4 @@
-package com.ljm.server.protocol.listener;
+package com.ljm.server.protocol.handler;
 
 import com.ljm.server.demo.FileTransfer;
 import com.ljm.server.event.handler.AbstractEventHandler;
@@ -8,20 +8,18 @@ import com.ljm.server.protocol.http.HttpRequestMessage;
 import com.ljm.server.protocol.http.parser.AbstractHttpRequestMessageParser;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 /**
  * @author 李佳明 https://github.com/pkpk1234
  * @date 2018-01-2018/1/8
  */
-public class HttpEventHandler extends AbstractEventHandler<Connection> {
+public class HttpStaticResourceEventHandler extends AbstractEventHandler<Connection> {
 
     private final String docBase;
     private final FileTransfer fileTransfer = new FileTransfer();
     private final AbstractHttpRequestMessageParser httpRequestMessageParser;
 
-    public HttpEventHandler(String docBase, AbstractHttpRequestMessageParser httpRequestMessageParser) {
+    public HttpStaticResourceEventHandler(String docBase, AbstractHttpRequestMessageParser httpRequestMessageParser) {
         this.docBase = docBase;
         this.httpRequestMessageParser = httpRequestMessageParser;
     }
@@ -31,21 +29,12 @@ public class HttpEventHandler extends AbstractEventHandler<Connection> {
 
         try {
             HttpRequestMessage httpRequestMessage = httpRequestMessageParser.parse(connection.getInputStream());
+            String path = httpRequestMessage.getRequestLine().getRequestURI().getPath();
             
         } catch (IOException e) {
             throw new HandlerException(e);
         }
     }
 
-    /**
-     * 返回文件
-     *
-     * @param inputstream
-     * @param outputStream
-     */
-    private void getFile(String docBase, InputStream inputstream,
-                         OutputStream outputStream) {
-        fileTransfer.getFile(docBase, inputstream, outputStream);
-    }
 
 }
